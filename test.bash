efe7b0446a2037640937da87e983f14bfb3275af
patiltussh <33482850+patiltussh@users.noreply.github.com>
2020-06-30 16:44:44 +0530

    Update test.bash

--- test.bash
+++ test.bash
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+salt_env=$1
+pvt_key=$2
+nonprod=( "dev1" "dev2" )
+prod=( "prod1" "prod2" )
+
+if [ $salt_env == "non-prod" ]
+then
+  for host in "${nonprod[@]}"; do
+    echo $host
+    echo $pvt_key
+  done
+else
+  for host in "${prod[@]}"; do
+    echo ${host}
+  done
+fi
 
